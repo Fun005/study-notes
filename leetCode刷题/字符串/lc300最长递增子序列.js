2089408abb4304c4ffa57b22137d003f5c25c6e8
tangc1 <tangci@mgtv.com>
2025-08-02 17:43:39 +0800

    ✨ 最长递增子序列&判断是否子序列

--- leetCode刷题/字符串/lc300最长递增子序列.js
+++ leetCode刷题/字符串/lc300最长递增子序列.js
@@ -3,7 +3,6 @@
 
     子序列 是由数组派生而来的序列，删除（或不删除）数组中的元素而不改变其余元素的顺序。例如，[3,6,2,7] 是数组 [0,3,1,6,2,2,7] 的子序列。
 
-
     示例 1：
 
     输入：nums = [10,9,2,5,3,7,101,18]
@@ -18,21 +17,56 @@
     输入：nums = [7,7,7,7,7,7,7]
     输出：1
 
-
     提示：
 
     1 <= nums.length <= 2500
     -104 <= nums[i] <= 104
 
-
     进阶：
 
     你能将算法的时间复杂度降低到 O(n log(n)) 吗?
  */
+
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var lengthOfLIS = function (nums) {
+    if (nums.length === 0) return 0;
 
+    const tails = [nums[0]]
+
+    // 当遍历到的数字大于 tails 数组的最后一个元素时，直接添加到 tails 数组中
+    // 否则在 tails 数组中找到第一个比遍历到的数字大的元素,将其替换成遍历到的数字
+    for (let i = 0; i < nums.length; i++) {
+
+        if (nums[i] > tails[tails.length - 1]) {
+            tails.push(nums[i])
+        } else {
+            const index = getFirstBiggerIndex(nums[i])
+            tails[index] = nums[i]
+        }
+    }
+    return tails.length;
+
+    function getFirstBiggerIndex(num) {
+        // for (let j = 0; j < tails.length; j++) {
+        //     if (tails[j] >= num) {
+        //         return j
+        //     }
+        // }
+
+
+        // 用二分查找优化
+        let i = 0, j = tails.length - 1;
+        while (i < j) {
+            const mid = Math.floor((i + j) / 2);
+            if (tails[mid] >= num) {
+                j = mid;
+            } else {
+                i = mid + 1;
+            }
+        }
+        return i;
+    }
 };
---
+++ leetCode刷题/字符串/lc392判断子序列.js
@@ -0,0 +1,58 @@
+/**
+    给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+
+    字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。（例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+
+    进阶：
+
+    如果有大量输入的 S，称作 S1, S2, ... , Sk 其中 k >= 10亿，你需要依次检查它们是否为 T 的子序列。在这种情况下，你会怎样改变代码？
+
+
+    示例 1：
+
+    输入：s = "abc", t = "ahbgdc"
+    输出：true
+    示例 2：
+
+    输入：s = "axc", t = "ahbgdc"
+    输出：false
+
+
+    提示：
+
+    0 <= s.length <= 100
+    0 <= t.length <= 10^4
+    两个字符串都只由小写字符组成。
+ */
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isSubsequence = function (s, t) {
+    if (s === "") return true
+    // 双指针
+    // i 遍历 s, j 遍历 t
+    let i = 0, j = 0;
+    while (i < s.length && j < t.length) {
+        if (s[i] === t[j] && ++i >= s.length) {
+            return true
+        } else {
+            i++
+            j++
+        }
+    }
+
+    return false
+};
+
+var isSubsequence2 = function (s, t) {
+    if (s === "") return true
+    for (let i = 0, j = 0; j < t.length; j++) {
+        if (s[i] === t[j] && ++i >= s.length) {
+            return true
+        }
+    }
+    return false
+}
