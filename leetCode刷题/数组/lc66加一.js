0167b3dfa03f8ad408e4882129a9e27ac1b4ed3d
tangc1 <tangci@mgtv.com>
2025-08-04 22:47:23 +0800

    ✨ plusone

--- leetCode刷题/数组/lc66加一.js
+++ leetCode刷题/数组/lc66加一.js
@@ -39,7 +39,28 @@
  * @param {number[]} digits
  * @return {number[]}
  */
-var plusOne = function (digits) {
+
+/**
+    一个整数加1，无论进不进位，都是从尾部开始加1的
+    不进位
+        末尾加1 < 10，加后直接返回，对应数组末尾元素+1即可
+    进位
+        对需要进位的原数组中的位的数修改值为0
+        进位位数 < 整数位数
+            数组相应位置元素+1即可
+            何时+1？
+                ==9
+                即加1后模10为0，说明原位数字为9
+        进位位数 = 整数位数
+            例如9，99，999，9999
+            此时直接变为10，100，1000，10000即可
+                在遍历中修改
+                    判断是否能遍历到整数的位数，如果能，原数组直接左边插入1个1即可
+                在结果中修改
+                    直接重新新建一个原数组长度加1长度的新数组，设置首元素为1，其余元素为0，返回新数组即可
+ */
+
+var plusOne1 = function (digits) {
     const len = digits.length
     for (i = len - 1; i >= 0; i--) {
         digits[i] = (digits[i] + 1) % 10
@@ -47,6 +68,34 @@ var plusOne = function (digits) {
     }
 
     digits.unshift(1)
-    digits[0] = 1
     return digits
 };
+
+var plusOne2 = function (digits) {
+    for (let i = digits.lnegth - 1; i >= 0; i--) {
+        if (digits[i] === 9) {
+            digits[i] = 0
+        } else {
+            digits[i] += 1
+            return digits
+        }
+    }
+    digits.unshift(1)
+    return digits
+}
+
+var plusOne3 = function (digits) {
+    const len = digits.length
+    digits[len - 1]++
+    for (let i = len - 1; i >= 0; i--) {
+        if (digits[i] === 10) {
+            digits[i] = 0
+            if (i === 0) {
+                digits.unshift(1)
+            } else {
+                digits[i - 1]++
+            }
+        }
+    }
+    return digits
+}
